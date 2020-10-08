d32b850c3682e682244b30816d03cd3bd9c96d5a
vasundhara005 <vasundhrasiriki@gmail.com>
2020-10-08 07:33:12 +0530

    Updates

---
+++ precache-manifest.a85874bdd83f265dc430a77e6f223bb1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "507f784dcf0ad9dd18e3bb37ef190a84",
+    "url": "/reactdata/index.html"
+  },
+  {
+    "revision": "edd9db74eadbb3e9db8a",
+    "url": "/reactdata/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "5415a69a2e3e3482a4e0",
+    "url": "/reactdata/static/css/main.1e8e27cf.chunk.css"
+  },
+  {
+    "revision": "edd9db74eadbb3e9db8a",
+    "url": "/reactdata/static/js/2.beaddc98.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/reactdata/static/js/2.beaddc98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5415a69a2e3e3482a4e0",
+    "url": "/reactdata/static/js/main.26294fdd.chunk.js"
+  },
+  {
+    "revision": "066bab181407c4c743a7",
+    "url": "/reactdata/static/js/runtime-main.51ea4084.js"
+  }
+]);
